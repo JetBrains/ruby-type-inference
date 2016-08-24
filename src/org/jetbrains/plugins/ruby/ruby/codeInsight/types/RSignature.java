d9d17641145f6bd952df715cfcf83ad9165c62ab
Mikhail.Chernyavsky <Mikhail.Chernyavsky@jetbrains.com>
2016-08-24 19:45:38 +0300

    Added gemName and GemVersion fields to RSignature

--- src/org/jetbrains/plugins/ruby/ruby/codeInsight/types/RSignature.java
+++ src/org/jetbrains/plugins/ruby/ruby/codeInsight/types/RSignature.java
@@ -7,23 +7,30 @@ import java.util.List;
 
 class RSignature {
     @NotNull
-    private final String myMethodName;
+    private String myMethodName;
     @NotNull
-    private final String myReceiverName;
+    private String myReceiverName;
     @NotNull
     private final Visibility myVisibility;
     @NotNull
     private final List<ParameterInfo> myArgsInfo;
     @NotNull
     private final List<String> myArgsTypeName;
+    @NotNull
+    private final String myGemName;
+    @NotNull
+    private final String myGemVersion;
 
     RSignature(@NotNull final String methodName, @NotNull final String receiverName, @NotNull final Visibility visibility,
-               @NotNull final List<ParameterInfo> argsInfo, @NotNull final List<String> argsTypeName) {
+               @NotNull final List<ParameterInfo> argsInfo, @NotNull final List<String> argsTypeName,
+               @NotNull final String gemName, @NotNull final String gemVersion) {
         this.myMethodName = methodName;
         this.myReceiverName = receiverName;
         this.myVisibility = visibility;
         this.myArgsInfo = argsInfo;
         this.myArgsTypeName = argsTypeName;
+        this.myGemName = gemName;
+        this.myGemVersion = gemVersion;
     }
 
     @NotNull
@@ -49,6 +56,16 @@ class RSignature {
     @NotNull
     List<String> getArgsTypeName() {
         return myArgsTypeName;
+    }
+
+    @NotNull
+    String getGemName() {
+        return myGemName;
+    }
+
+    @NotNull
+    String getGemVersion() {
+        return myGemVersion;
     }
 
     @Override
