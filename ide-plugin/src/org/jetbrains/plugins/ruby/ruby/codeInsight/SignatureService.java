38a8c6caf581addceedb404261d192198f3efef2
Viuginov Nickolay <nickolay.viuginov@jetbrains.com>
2017-03-10 15:09:21 +0300

    added Exception logging in SignatureService

--- ide-plugin/src/org/jetbrains/plugins/ruby/ruby/codeInsight/SignatureService.java
+++ ide-plugin/src/org/jetbrains/plugins/ruby/ruby/codeInsight/SignatureService.java
@@ -2,9 +2,11 @@ package org.jetbrains.plugins.ruby.ruby.codeInsight;
 
 import org.jetbrains.ruby.runtime.signature.server.SignatureServer;
 
+import java.util.logging.Logger;
+
 
 public class SignatureService {
-
+    private static final Logger LOGGER = Logger.getLogger("SignatureService");
     public SignatureService() {
         new Thread() {
             @Override
@@ -13,7 +15,8 @@ public class SignatureService {
                 try {
                     server.runServer();
                 } catch (Exception e) {
-
+                    LOGGER.severe(e.getMessage());
+                    e.printStackTrace();
                 }
 
             }
