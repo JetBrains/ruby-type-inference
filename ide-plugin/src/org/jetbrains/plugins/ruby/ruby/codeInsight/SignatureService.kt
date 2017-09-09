084a1997d24aa7892d0d69c4456e70b0c053e8db
Valentin Fondaratov <fondarat@gmail.com>
2017-09-09 20:16:15 +0300

    remove unneeded dir creation and add useSSL=false to silence warnings

--- ide-plugin/src/org/jetbrains/plugins/ruby/ruby/codeInsight/SignatureService.kt
+++ ide-plugin/src/org/jetbrains/plugins/ruby/ruby/codeInsight/SignatureService.kt
@@ -1,8 +1,6 @@
 package org.jetbrains.plugins.ruby.ruby.codeInsight
 
-import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.util.io.FileUtil
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -11,23 +9,11 @@ import org.jetbrains.ruby.codeInsight.types.storage.server.impl.GemInfoTable
 import org.jetbrains.ruby.codeInsight.types.storage.server.impl.MethodInfoTable
 import org.jetbrains.ruby.codeInsight.types.storage.server.impl.SignatureTable
 import org.jetbrains.ruby.runtime.signature.server.SignatureServer
-import java.io.File
-import java.io.IOException
 
 
 class SignatureService {
     init {
-        val dbDir = FileUtil.join(PathManager.getSystemPath(), "ruby_runtime_stat")
-        val isCreated = File(dbDir).mkdir()
-        if (isCreated) {
-            LOGGER.info("Created dir for db: " + dbDir)
-        }
-
-        if (!File(dbDir).exists()) {
-            throw RuntimeException(IOException("Dir for the db does not exist and could not be created: " + dbDir))
-        }
-
-        Database.connect("jdbc:mysql://localhost:3306/" + "ruby_type_contracts" + "?serverTimezone=UTC&nullNamePatternMatchesAll=true",
+        Database.connect("jdbc:mysql://localhost:3306/" + "ruby_type_contracts" + "?serverTimezone=UTC&nullNamePatternMatchesAll=true&useSSL=false",
                 driver = "com.mysql.cj.jdbc.Driver", user = "rubymine", password = "rubymine")
         transaction {
             SchemaUtils.create(GemInfoTable, ClassInfoTable, MethodInfoTable, SignatureTable)
