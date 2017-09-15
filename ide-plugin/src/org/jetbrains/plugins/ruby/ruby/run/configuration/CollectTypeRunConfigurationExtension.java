0f10bb9032ee961084f73b1b24a40a06429a1860
Valentin Fondaratov <fondarat@gmail.com>
2017-09-15 07:51:27 +0300

    ask to install arg_scanner gem on execution; pass src roots to gem
    The way gem is installed is almost the same as it is for debugger gems.
    The difference is that we don't bundle arg_scanner gem with the plugin.
    
    Modules content roots are passed to the gem to distinguish them from
    "fixed" gems code. See b52eff2e76d5408353099064b03938bd584ceaf8.

--- ide-plugin/src/org/jetbrains/plugins/ruby/ruby/run/configuration/CollectTypeRunConfigurationExtension.java
+++ ide-plugin/src/org/jetbrains/plugins/ruby/ruby/run/configuration/CollectTypeRunConfigurationExtension.java
@@ -5,6 +5,9 @@ import com.intellij.execution.configurations.GeneralCommandLine;
 import com.intellij.execution.configurations.RunnerSettings;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.options.SettingsEditor;
+import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.InvalidDataException;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -12,16 +15,23 @@ import org.jdom.Element;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.plugins.ruby.gem.GemDependency;
 import org.jetbrains.plugins.ruby.gem.GemInfo;
+import org.jetbrains.plugins.ruby.gem.GemInstallUtil;
 import org.jetbrains.plugins.ruby.gem.util.GemSearchUtil;
+import org.jetbrains.plugins.ruby.ruby.RModuleUtil;
 import org.jetbrains.plugins.ruby.ruby.RubyUtil;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class CollectTypeRunConfigurationExtension extends RubyRunConfigurationExtension {
     private static final String ARG_SCANNER_GEM_NAME = "arg_scanner";
 
     private static final String ARG_SCANNER_REQUIRE_SCRIPT = "arg_scanner/starter";
+
+    private static final String ROOTS_ENV_KEY = "ARG_SCANNER_PROJECT_ROOTS";
 
     @NonNls
     @NotNull
@@ -61,7 +71,7 @@ public class CollectTypeRunConfigurationExtension extends RubyRunConfigurationEx
             return;
         }
 
-        String includeKey = getRequireKeyForGem(module, ARG_SCANNER_GEM_NAME);
+        String includeKey = getRequireKeyForGem(module, configuration.getSdk(), ARG_SCANNER_GEM_NAME);
         if (includeKey == null) {
             return;
         }
@@ -70,27 +80,54 @@ public class CollectTypeRunConfigurationExtension extends RubyRunConfigurationEx
         final String rubyOpt = StringUtil.notNullize(env.get(RubyUtil.RUBYOPT));
         final String newRubyOpt = rubyOpt + includeKey + " -r" + ARG_SCANNER_REQUIRE_SCRIPT;
 
+        final Module[] rubyModules = RModuleUtil.getInstance().getAllModulesWithRubySupport(configuration.getProject());
+        final String localCodeRoots = StringUtil.join(rubyModules, it -> {
+            final VirtualFile[] contentRoots = ModuleRootManager.getInstance(it).getContentRoots();
+            return StringUtil.join(contentRoots, VirtualFile::getPath, ":");
+        }, ":");
+        env.put(ROOTS_ENV_KEY, localCodeRoots);
+
         cmdLine.withEnvironment(RubyUtil.RUBYOPT, newRubyOpt);
     }
 
     @Override
     protected void validateConfiguration(@NotNull AbstractRubyRunConfiguration configuration, boolean isExecution) throws Exception {
         RunConfigurationUtil.inspectSDK(configuration, isExecution);
-        Module module = configuration.getModule();
-        if (module == null) {
+        final Module module = configuration.getModule();
+        final Sdk sdk = configuration.getSdk();
+        if (module == null || sdk == null) {
             RunConfigurationUtil.throwExecutionOrRuntimeException("Cannot execute outside of module context", isExecution);
         }
 
-        GemInfo argScannerGem = GemSearchUtil.findGem(module, ARG_SCANNER_GEM_NAME);
+        GemInfo argScannerGem = GemSearchUtil.findGem(module, sdk, ARG_SCANNER_GEM_NAME);
         if (argScannerGem == null) {
+            if (isExecution) {
+                final int result = Messages.showYesNoDialog(configuration.getProject(),
+                        "'arg_scanner' gem is required to collect the data. Do you want to install it?",
+                        "Gem Not Found", null);
+                if (result == Messages.YES) {
+                    final HashMap<GemDependency, String> errors = new HashMap<>();
+                    GemInstallUtil.installGemsDependencies(sdk,
+                            module,
+                            Collections.singleton(GemDependency.create(ARG_SCANNER_GEM_NAME, ">= 0.1.9")),
+                            true,
+                            false,
+                            errors);
+                    if (errors.isEmpty()) {
+                        // means success
+                        return;
+                    }
+                }
+            }
+
             RunConfigurationUtil.throwExecutionOrRuntimeException("Cannot find required " + ARG_SCANNER_GEM_NAME +
                     " gem in the current SDK", false);
         }
     }
 
     @Nullable
-    private String getRequireKeyForGem(@NotNull Module module, @NotNull String gemName) {
-        final GemInfo gemInfo = GemSearchUtil.findGem(module, gemName);
+    private String getRequireKeyForGem(@NotNull Module module, @Nullable Sdk sdk, @NotNull String gemName) {
+        final GemInfo gemInfo = GemSearchUtil.findGem(module, sdk, gemName);
         if (gemInfo == null) {
             return null;
         }
