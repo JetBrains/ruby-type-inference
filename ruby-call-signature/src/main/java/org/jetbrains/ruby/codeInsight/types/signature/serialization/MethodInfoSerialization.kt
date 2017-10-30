94bcdf0b6decaccd244aa8405c3566d5d6af0671
Vladimir Koshelev <Vladimir.Koshelev@jetbrains.com>
2017-10-30 13:59:16 +0300

    Refactor `SignatureInfoSerialization`

--- ruby-call-signature/src/main/java/org/jetbrains/ruby/codeInsight/types/signature/serialization/MethodInfoSerialization.kt
+++ ruby-call-signature/src/main/java/org/jetbrains/ruby/codeInsight/types/signature/serialization/MethodInfoSerialization.kt
@@ -62,9 +62,40 @@ fun Location(stream: DataInput): Location {
 
 object SignatureInfoSerialization {
 
-    val PROTOCOL_VERSION = 1
+    private val PROTOCOL_VERSION = 1
 
     fun serialize(signatureInfos: List<SignatureInfo>, stream: DataOutput) {
+        writeProtocolVersion(stream)
+
+        val (classInfo2Id, gemInfo2Id) = collectClassInfoAndGemInfo(signatureInfos)
+        serializeGemInfos(gemInfo2Id, stream)
+        serializeClassInfos(stream, classInfo2Id, gemInfo2Id)
+        serializeSignatureInfos(stream, signatureInfos, classInfo2Id)
+    }
+
+    fun deserialize(stream: DataInput): List<SignatureInfo> {
+        checkProtocolVersion(stream)
+
+        val id2GemInfo = deserializeGemInfo(stream)
+        val id2ClassInfo = deserializeClassInfo(stream, id2GemInfo)
+        return deserializeSignatureInfos(stream, id2ClassInfo)
+    }
+
+    private fun writeProtocolVersion(stream: DataOutput) {
+        stream.writeInt(PROTOCOL_VERSION)
+    }
+
+
+    private fun checkProtocolVersion(stream: DataInput) {
+        val version = stream.readInt()
+        if (version != PROTOCOL_VERSION) {
+            throw IOException("Cannot deserialize SignatureInfos: protocol version mismatch. Expected:" +
+                    " $PROTOCOL_VERSION but got: $version")
+        }
+    }
+
+    private fun collectClassInfoAndGemInfo(signatureInfos: List<SignatureInfo>) :
+            Pair<LinkedHashMap<ClassInfo, Int>, LinkedHashMap<GemInfo, Int>>{
         val gemInfo2Id = LinkedHashMap<GemInfo, Int>()
         val classInfo2Id = LinkedHashMap<ClassInfo, Int>()
         signatureInfos.forEach {
@@ -75,21 +106,53 @@ object SignatureInfoSerialization {
                 gemInfo2Id.putIfAbsent(gemInfo, gemInfo2Id.size)
             }
         }
-        stream.writeInt(PROTOCOL_VERSION)
+        return Pair(classInfo2Id, gemInfo2Id)
+    }
+
+    private fun serializeGemInfos(gemInfo2Id: LinkedHashMap<GemInfo, Int>, stream: DataOutput) {
         stream.writeInt(gemInfo2Id.size)
         var iter = 0
         gemInfo2Id.forEach {
             assert(iter++ == it.value)
             it.key.serialize(stream)
         }
+    }
+
+    private fun deserializeGemInfo(stream: DataInput): LinkedHashMap<Int, GemInfo> {
+        val id2GemInfo = LinkedHashMap<Int, GemInfo>()
+        val gemInfoSize = stream.readInt()
+        for (i in 1..gemInfoSize) {
+            val gemInfo = GemInfo(stream)!!
+            id2GemInfo.put(i - 1, gemInfo)
+        }
+        id2GemInfo.put(-1, GemInfo.NONE)
+        return id2GemInfo
+    }
+
+    private fun serializeClassInfos(stream: DataOutput, classInfo2Id: LinkedHashMap<ClassInfo, Int>, gemInfo2Id: LinkedHashMap<GemInfo, Int>) {
         gemInfo2Id.put(GemInfo.NONE, -1)
+        var iter = 0
         stream.writeInt(classInfo2Id.size)
-        iter = 0
         classInfo2Id.forEach {
             assert(iter++ == it.value)
             stream.writeUTF(it.key.classFQN)
             stream.writeInt(gemInfo2Id.getValue(it.key.gemInfo ?: GemInfo.NONE))
         }
+    }
+
+    private fun deserializeClassInfo(stream: DataInput,
+                                     id2GemInfo: LinkedHashMap<Int, GemInfo>): LinkedHashMap<Int, ClassInfo> {
+        val id2ClassInfo = LinkedHashMap<Int, ClassInfo>()
+        val classInfoSize = stream.readInt()
+        for (i in 1..classInfoSize) {
+            val fqn = stream.readUTF()
+            val gemInfo = id2GemInfo.getValue(stream.readInt())
+            id2ClassInfo.put(i - 1, ClassInfo(gemInfo, fqn))
+        }
+        return id2ClassInfo
+    }
+
+    private fun serializeSignatureInfos(stream: DataOutput, signatureInfos: List<SignatureInfo>, classInfo2Id: LinkedHashMap<ClassInfo, Int>) {
         stream.writeInt(signatureInfos.size)
         signatureInfos.forEach {
             val methodInfo = it.methodInfo
@@ -102,27 +165,9 @@ object SignatureInfoSerialization {
         }
     }
 
-    fun deserialize(stream: DataInput): List<SignatureInfo> {
+    private fun deserializeSignatureInfos(stream: DataInput,
+                                          id2ClassInfo: LinkedHashMap<Int, ClassInfo>) : List<SignatureInfo> {
         val result = ArrayList<SignatureInfo>()
-        val version = stream.readInt()
-        if (version != PROTOCOL_VERSION) {
-            throw IOException("Cannot deserialize SignatureInfos: protocol version mismatch. Expected:" +
-                    " $PROTOCOL_VERSION but got: $version")
-        }
-        val id2GemInfo = LinkedHashMap<Int, GemInfo>()
-        val id2ClassInfo = LinkedHashMap<Int, ClassInfo>()
-        val gemInfoSize = stream.readInt()
-        for (i in 1..gemInfoSize) {
-            val gemInfo = GemInfo(stream)!!
-            id2GemInfo.put(i - 1, gemInfo)
-        }
-        id2GemInfo.put(-1, GemInfo.NONE)
-        val classInfoSize = stream.readInt()
-        for (i in 1..classInfoSize) {
-            val fqn = stream.readUTF()
-            val gemInfo = id2GemInfo.getValue(stream.readInt())
-            id2ClassInfo.put(i - 1, ClassInfo(gemInfo, fqn))
-        }
         val signatureInfoSize = stream.readInt()
         for (i in 1..signatureInfoSize) {
             val name = stream.readUTF()
