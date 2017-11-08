e0b861aaf594192b124951bb67e18195a6a79ab1
Vladimir Koshelev <vedun-z@yandex.ru>
2017-11-08 23:01:07 +0300

    Use `require_relative` in starter.rb

--- arg_scanner/lib/arg_scanner/starter.rb
+++ arg_scanner/lib/arg_scanner/starter.rb
@@ -1,4 +1,5 @@
-require 'arg_scanner'
+require_relative 'arg_scanner'
+require_relative 'type_tracker'
 
 # instantiating type tracker will enable calls tracing and sending the data
 ArgScanner::TypeTracker.instance
