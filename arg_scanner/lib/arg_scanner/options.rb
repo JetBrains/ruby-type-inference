9399ebba2168c2f32d5ca8d5b74c7521634dd755
Nikita Bobko <nikitabobko@gmail.com>
2018-08-02 12:54:09 +0300

    Remove --roots, --local=[VERSION], --no-local arg-scanner arguments

--- arg_scanner/lib/arg_scanner/options.rb
+++ arg_scanner/lib/arg_scanner/options.rb
@@ -2,9 +2,6 @@ require 'ostruct'
 
 module ArgScanner
   OPTIONS = OpenStruct.new(
-      :local_version => ENV['ARG_SCANNER_LOCAL_VERSION'] || '0',
-      :no_local => ENV['ARG_SCANNER_NO_LOCAL'] ? true : false,
-      :project_roots => ((ENV['ARG_SCANNER_PROJECT_ROOTS'] || "").split ':'),
       :enable_type_tracker => ENV['ARG_SCANNER_ENABLE_TYPE_TRACKER'],
       :enable_state_tracker => ENV['ARG_SCANNER_ENABLE_STATE_TRACKER'],
       :enable_return_type_tracker => ENV['ARG_SCANNER_ENABLE_RETURN_TYPE_TRACKER'],
@@ -13,9 +10,6 @@ module ArgScanner
   )
 
   def OPTIONS.set_env
-    ENV['ARG_SCANNER_LOCAL_VERSION'] = self.local_version.to_s
-    ENV['ARG_SCANNER_NO_LOCAL'] = self.no_local ? "1" : nil
-    ENV['ARG_SCANNER_PROJECT_ROOTS'] = self.project_roots.join ':'
     ENV['ARG_SCANNER_ENABLE_TYPE_TRACKER'] = self.enable_type_tracker ? "1" : nil
     ENV['ARG_SCANNER_ENABLE_STATE_TRACKER'] = self.enable_state_tracker ? "1" : nil
     ENV['ARG_SCANNER_ENABLE_RETURN_TYPE_TRACKER'] = self.enable_return_type_tracker ? "1" : nil
