e3c434b237872615e43732291a80555b059e69c0
Nikita Bobko <nikitabobko@gmail.com>
2018-10-16 20:25:43 +0300

    Add DBViewer for simplifying H2 db viewing

---
+++ signature-viewer/src/org/jetbrains/ruby/runtime/signature/DBViewer.kt
@@ -0,0 +1,39 @@
+package org.jetbrains.ruby.runtime.signature
+
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.ruby.codeInsight.types.storage.server.DatabaseProvider
+import org.jetbrains.ruby.codeInsight.types.storage.server.impl.CallInfoRow
+import org.jetbrains.ruby.codeInsight.types.storage.server.impl.CallInfoTable
+
+/**
+ * Just prints content of [CallInfoTable]
+ */
+fun main(args: Array<String>) {
+    val dpPath = parseDBViewerCommandLineArgs(args)
+    DatabaseProvider.connectToDB(dpPath)
+
+    transaction {
+        val table = CallInfoRow.all().map { it.copy() }
+        table.forEach {
+            println("" +
+                    (it.methodInfo.classInfo.gemInfo?.name ?: "No gem") + " " +
+                    (it.methodInfo.classInfo.gemInfo?.version ?: "No gem") + " " +
+                    it.methodInfo.location?.path + " " +
+                    it.methodInfo.location?.lineno + " " +
+                    it.methodInfo.visibility + " " +
+                    it.methodInfo.classInfo.classFQN + " " +
+                    it.methodInfo.name + " " +
+                    "args:${it.argumentsTypesJoinToString()} " +
+                    "return:${it.returnType}")
+        }
+        println("Size: ${table.size}")
+    }
+}
+
+fun parseDBViewerCommandLineArgs(args: Array<String>): String {
+    if (args.size != 1) {
+        println("Usage: <db-file-path>")
+        System.exit(1)
+    }
+    return args.single()
+}
