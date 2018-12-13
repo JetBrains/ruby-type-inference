304412fc932552c385c554e8b2d558f091e7b4f0
Nikita Bobko <nikitabobko@gmail.com>
2018-12-13 20:02:27 +0300

    Write to logs DB file path

--- ide-plugin/src/org/jetbrains/plugins/ruby/ruby/codeInsight/ConnectToDBActivity.kt
+++ ide-plugin/src/org/jetbrains/plugins/ruby/ruby/codeInsight/ConnectToDBActivity.kt
@@ -1,5 +1,6 @@
 package org.jetbrains.plugins.ruby.ruby.codeInsight
 
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
@@ -8,10 +9,16 @@ import org.jetbrains.ruby.codeInsight.types.storage.server.DatabaseProvider
 import java.nio.file.Paths
 
 class ConnectToDBActivity : StartupActivity, DumbAware {
+    val log = Logger.getInstance(this.javaClass.canonicalName)
+
     override fun runActivity(project: Project) {
-        DatabaseProvider.connectToDB(Paths.get(
+        val filePath = Paths.get(
                 TypeInferenceDirectory.RUBY_TYPE_INFERENCE_DIRECTORY.toString(),
-                project.name).toString())
+                project.name).toString()
+
+
+        DatabaseProvider.connectToDB(filePath)
+        log.info("Connected to DB: $filePath")
         DatabaseProvider.createAllDatabases()
     }
 }
